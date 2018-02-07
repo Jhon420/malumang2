404e37b423eee00654d60221b4b8168545d3f1a1
Opilane TTHK <opilane@tthk.ee>
2018-02-07 15:35:31 +0200

    Initial commit

---
+++ app/src/main/java/com/example/opilane/malumang/StartActivity.java
@@ -0,0 +1,8 @@
+package com.example.opilane.malumang;
+
+
+import android.support.v7.app.AppCompatActivity;
+
+public class StartActivity extends AppCompatActivity {
+}
+
---
+++ app/src/main/java/com/example/opilane/malumang/mainActivity.java
@@ -0,0 +1,8 @@
+package com.example.opilane.malumang;
+
+/**
+ * Created by opilane on 07.02.2018.
+ */
+
+public class mainActivity {
+}
